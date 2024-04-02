9c9959675a8b94ab419422e70251b04bf859325c
titi1987 <45232758+titi1987@users.noreply.github.com>
2024-04-02 23:34:27 +0100

    Add files via upload

---
+++ Check_Intune_Logs.ps1
@@ -0,0 +1,12 @@
+﻿# Define the log file path
+$logFilePath = "D:\INTUNE\IntuneManagementExtension.log"
+
+# Define the output CSV file path
+$outputCsvPath = "C:\global\IntuneErrors.csv"
+
+# Search for "error code" and export to CSV
+Select-String -Path $logFilePath -Pattern "error code" -CaseSensitive:$false |
+    Select-Object LineNumber, Line |
+    Export-Csv -Path $outputCsvPath -NoTypeInformation -Force
+
+Write-Host "Operation completed. Check '$outputCsvPath' for results."
