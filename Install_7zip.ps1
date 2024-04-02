6a1e91c5b0f87e52a7174ae56a3625337cefa4cc
titi1987 <45232758+titi1987@users.noreply.github.com>
2024-04-02 22:59:06 +0100

    Add files via upload

---
+++ Install_7zip.ps1
@@ -0,0 +1,14 @@
+﻿# Define variables
+$pickliLogs = "c:\programdata\pickliLogs"
+$pickliScriptRoot = Split-Path -Path $MyInvocation.MyCommand.Path
+$pickliInstallFile = "$pickliScriptRoot\7z2403-x64.msi"
+$pickliArguments = "/qn /l `"$pickliLogs\Install-7z2403-x64.log`" /norestart"
+
+# Check if log folder exists, if not, create it
+if(!(Test-Path $pickliLogs))
+{
+    New-Item -ItemType Directory -Path $pickliLogs -Force
+}
+
+# Install 7-Zip
+Start-Process -FilePath $pickliInstallFile -ArgumentList $pickliArguments -Wait
