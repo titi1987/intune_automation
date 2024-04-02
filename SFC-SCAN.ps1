60de1ffb3af73cd2502d43f23230e747ee8abd0e
titi1987 <45232758+titi1987@users.noreply.github.com>
2024-04-02 23:47:20 +0100

    Add files via upload

---
+++ SFC-SCAN.ps1
@@ -0,0 +1,10 @@
+﻿#Setup Trigger, Task, Settings
+
+$pickliTrigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Friday -At 17:00pm
+$pickliAction = New-ScheduledTaskAction -Execute "SFC" -Argument "/Scannow"
+$pickliSetting = New-ScheduledTaskSettingsSet -Compatibility Win8 -AllowStartIfOnBatteries -StartWhenAvailable -DontStopIfGoingOnBatteries
+
+$pickliTask = New-ScheduledTask -Trigger $pickliTrigger -Action $pickliAction -Settings $pickliSetting -Description "System File Checker weekly Fridays run at 5pm"
+
+#Create/Register Scheduled Task
+Register-ScheduledTask -TaskPath \PICKLI\ -TaskName "System File Checker weekly Fridays run at 5pm" -InputObject $pickliTask -User "NT AUTHORITY\SYSTEM"
