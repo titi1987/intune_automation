8ad8e50084c23df38caaff5c9654190b65d427c3
titi1987 <45232758+titi1987@users.noreply.github.com>
2024-04-03 17:45:41 +0100

    Add files via upload

---
+++ RegKey_detection.ps1
@@ -0,0 +1,36 @@
+﻿# Variables
+$regkey1 = "HKLM:\Software\Microsoft\Security Center\Feature"
+$regkey2 = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender"
+
+$name = "HideFileExt"
+$value = 1
+
+# Check if the first registry key exists
+if (!(Test-Path $regkey1)) {
+    Write-Output 'RegKey 1 not available - remediate'
+}
+else {
+    # Check the value of the registry key
+    $check = (Get-ItemProperty -Path $regkey1 -Name $name -ErrorAction SilentlyContinue).$name
+    if ($check -eq $value) {
+        Write-Output 'Setting in RegKey 1 OK - no remediation required'
+    }
+    else {
+        Write-Output 'Value in RegKey 1 not OK, go and remediate'
+    }
+}
+
+# Check if the second registry key exists
+if (!(Test-Path $regkey2)) {
+    Write-Output 'RegKey 2 not available - remediate'
+}
+else {
+    # Check the value of the registry key
+    $check = (Get-ItemProperty -Path $regkey2 -Name $name -ErrorAction SilentlyContinue).$name
+    if ($check -eq $value) {
+        Write-Output 'Setting in RegKey 2 OK - no remediation required'
+    }
+    else {
+        Write-Output 'Value in RegKey 2 not OK, go and remediate'
+    }
+}
---
+++ RegKey_remediation.ps1
@@ -0,0 +1,34 @@
+﻿# Variables
+$regkey1 = "HKLM:\Software\Microsoft\Security Center\Feature"
+$regkey2 = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender"
+
+$name = "HideFileExt"
+$value = 1
+
+# Remediation for Registry Key 1
+if (!(Test-Path $regkey1)) {
+    New-Item -Path $regkey1 -ErrorAction Stop
+}
+
+if (!(Get-ItemProperty -Path $regkey1 -Name $name -ErrorAction SilentlyContinue)) {
+    New-ItemProperty -Path $regkey1 -Name $name -Value $value -PropertyType DWORD -ErrorAction Stop
+    Write-Output "Remediation for $regkey1 complete"
+}
+else {
+    Set-ItemProperty -Path $regkey1 -Name $name -Value $value -ErrorAction Stop
+    Write-Output "Remediation for $regkey1 complete"
+}
+
+# Remediation for Registry Key 2
+if (!(Test-Path $regkey2)) {
+    New-Item -Path $regkey2 -ErrorAction Stop
+}
+
+if (!(Get-ItemProperty -Path $regkey2 -Name $name -ErrorAction SilentlyContinue)) {
+    New-ItemProperty -Path $regkey2 -Name $name -Value $value -PropertyType DWORD -ErrorAction Stop
+    Write-Output "Remediation for $regkey2 complete"
+}
+else {
+    Set-ItemProperty -Path $regkey2 -Name $name -Value $value -ErrorAction Stop
+    Write-Output "Remediation for $regkey2 complete"
+}
