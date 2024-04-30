b1d3733f446d8b2d4f69745ab9153b2dd19735b6
titi1987 <45232758+titi1987@users.noreply.github.com>
2024-04-30 15:17:17 +0100

    Update SFC-SCAN.ps1

--- SFC-SCAN.ps1
+++ SFC-SCAN.ps1
@@ -1,9 +1,13 @@
-﻿#Setup Trigger, Task, Settings
-
+# Create a new scheduled task trigger to run weekly on Fridays at 17:00 PM
 $pickliTrigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Friday -At 17:00pm
+
+# Create a new scheduled task action to execute the System File Checker with the "scannow" argument
 $pickliAction = New-ScheduledTaskAction -Execute "SFC" -Argument "/Scannow"
+
+# Set up the task settings: compatible with Windows 8 and newer, allows the task to start if on batteries, starts as soon as possible, and doesn't stop if switched to batteries
 $pickliSetting = New-ScheduledTaskSettingsSet -Compatibility Win8 -AllowStartIfOnBatteries -StartWhenAvailable -DontStopIfGoingOnBatteries
 
+# Combine the trigger, action, and settings into a new scheduled task with a description
 $pickliTask = New-ScheduledTask -Trigger $pickliTrigger -Action $pickliAction -Settings $pickliSetting -Description "System File Checker weekly Fridays run at 5pm"
 
 #Create/Register Scheduled Task
